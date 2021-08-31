a3b1d202d2634a032824e827144fe80f561478ea
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-08-31 13:00:16 +0300

    Login works (using hardcoded login)

--- views/Profile.js
+++ views/Profile.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
-import {Platform, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {Button, Platform, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Profile = (props) => {
+  const {isLoggedIn, setIsLoggedIn} = useContext(MainContext);
+  console.log('Profile isLoggedIn', isLoggedIn);
+  const logout = async () => {
+    setIsLoggedIn(false);
+    AsyncStorage.clear();
+    if (!isLoggedIn) {
+      // this is to make sure isLoggedIn has changed, will be removed later
+      props.navigation.navigate('Login');
+    }
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
+      <Button title={'Logout'} onPress={logout} />
     </SafeAreaView>
   );
 };
-
-Profile.propTypes = {};
 
 const styles = StyleSheet.create({
   container: {
@@ -21,5 +32,9 @@ const styles = StyleSheet.create({
     paddingTop: 40,
   },
 });
+
+Profile.propTypes = {
+  navigation: PropTypes.object,
+};
 
 export default Profile;
