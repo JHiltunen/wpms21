bb1f818a9e1762dcb1ba11e40ecfa0c3f173b416
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-09-02 09:35:48 +0300

    Added header and header image

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {Platform, SafeAreaView, StyleSheet, View} from 'react-native';
+import {ImageBackground, Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import List from './components/List';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.droidSafeArea}>
       <View style={styles.container}>
+        <View style={styles.header}>
+          <ImageBackground
+            source={{uri: 'https://placekitten.com/300/200'}}
+            resizeMode="cover"
+            style={styles.image}
+            imageStyle={styles.headerImageBackground}
+          ></ImageBackground>
+          <Text style={styles.kitten_slogan}>Lost and found kittens</Text>
+        </View>
         <List />
         <StatusBar style="auto" />
       </View>
@@ -16,13 +25,31 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#fff',
   },
-
+  image: {
+    flex: 1,
+  },
+  headerImageBackground: {
+    borderBottomRightRadius: 50,
+  },
+  kitten_slogan: {
+    width: 200,
+    height: 50,
+    backgroundColor: 'rgba(42, 215, 250, 0.4)',
+    position: 'absolute',
+    bottom: '20%',
+    left: 0,
+  },
   droidSafeArea: {
     flex: 1,
     backgroundColor: '#ccc',
     paddingTop: Platform.OS === 'android' ? 30 : 0,
+  },
+  header: {
+    flexBasis: 300,
+    position: 'relative',
   },
 });
 
