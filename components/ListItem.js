673d06b55e13492bafc36775d293dcd3576ce2c5
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-09-06 11:00:54 +0300

    Styled list item

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {uploadsUrl} from '../utils/variables';
+import {ActivityIndicator} from 'react-native';
+import {Button, Image} from 'react-native-elements';
 
 const ListItem = ({singleMedia, navigation}) => {
   return (
-    <TouchableOpacity
-      style={styles.row}
-      onPress={() => {
-        navigation.navigate('Single', singleMedia);
-      }}
-    >
+    <View style={styles.row}>
       <View style={styles.imagebox}>
         <Image
-          style={styles.image}
+          style={{width: 100, height: 100}}
           source={{uri: uploadsUrl + singleMedia.thumbnails?.w160}}
+          PlaceholderContent={<ActivityIndicator />}
         />
       </View>
       <View style={styles.textbox}>
         <Text style={styles.listTitle}>{singleMedia.title}</Text>
         <Text>{singleMedia.description}</Text>
+        <Button
+          raised
+          onPress={() => {
+            navigation.navigate('Single', singleMedia);
+          }}
+          title="View"
+        />
       </View>
-    </TouchableOpacity>
+    </View>
   );
 };
 
